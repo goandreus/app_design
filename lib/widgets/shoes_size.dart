536ef4db27edee6056090fa6284e812c83b6c6e1
goandreus <andreshtml5@gmail.com>
2020-06-04 19:15:01 -0500

    shadow box shadow

--- lib/widgets/shoes_size.dart
+++ lib/widgets/shoes_size.dart
@@ -33,8 +33,35 @@ class _ShoesShadow extends StatelessWidget {
       padding: const EdgeInsets.all(50.0),
       child: Stack(
         children: <Widget>[
+          Positioned(
+            bottom: 20,
+            right: 0,
+            child: _Shoes()),
           Image(image: AssetImage('assets/imgs/azul.png'),),
         ],
+      ),
+    );
+  }
+}
+
+class _Shoes extends StatelessWidget {
+  const _Shoes({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: -0.5,
+      child: Container(
+        width: 230,
+        height: 120,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          boxShadow: [
+            BoxShadow(color: Colors.orange[700], blurRadius: 40)
+          ]
+        ),
       ),
     );
   }
